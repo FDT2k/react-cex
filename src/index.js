96ccd29c294264e269a74d1ae3b569ab78348e30
Fabien Di Tore <fabien@ditore.ch>
2020-05-31 15:52:02 +0200

    Update index.js and yarn.lock
    Fixed for @geekagency/composite-js^2.x

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {map,compose,curry,reduce,_either,is_type_string,is_type_object,is_type_function,identity,keys,joinList} from '@geekagency/composite-js'
+import {map,compose,curry,reduce,either,is_type_string,is_type_object,is_type_function,identity,keys,joinList} from '@geekagency/composite-js'
 
 /*
 Easy class concat
@@ -25,21 +25,21 @@ const resolveObjectProp =  curry((object,prop) =>object[prop]() ? prop:'');
 
 const resolveObject = curry((separator,x)=> compose(trim,assemble(separator),remove_empty_items,map(resolveObjectProp(x)),keys)(x))
 
-const resolveFunction = x => _either(is_type_string, x=>'', identity)(x());
+const resolveFunction = x => either(is_type_string, x=>'', identity)(x());
 
 // defaultResolver :: String Object => string
 const conditionalResolver = separator=> compose(
-  _either(
+  either(
     is_type_function,
     identity,
     resolveFunction
   ),
-  _either(
+  either(
     is_type_object,
     identity,
     resolveObject(separator)
   ),
-  _either(
+  either(
     is_type_string,
     identity,
     resolveString,
